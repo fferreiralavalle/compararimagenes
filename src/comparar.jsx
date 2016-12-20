6b52eb3333601c4b3ed75477a4bad7033fa4ef08
Facundo Ferreira La Valle <facundo.ferreira@globant.com>
2016-12-20 18:49:08 -0300

    este anda

--- src/comparar.jsx
+++ src/comparar.jsx
@@ -1,9 +1,9 @@
 import React,{ Component } from 'react';
-import {compareImgs, getDistance, getBlackDotsCoord} from './compareImgs.jsx';
+import {compareImgs, getDistance, getBlackDotsCoord,getPosition} from './compareImgs.jsx';
 
 
 const imgOriginal = '/assets/messi_original.small.jpeg';
-const pelota = "/assets/messi_bocha.small.jpeg";
+const arrayImgs = ["/assets/messi_bocha.small.jpeg","assets/messi_cuerpo.small.jpeg"];
 const divisorUmbral = 20;
 
 export class Comparar extends Component{
@@ -12,17 +12,21 @@ export class Comparar extends Component{
       	return(
       		<div>
             Las imagenes a comparar son:<br/>
-            <canvas id="canvas" width="auto" height="auto"/>
-            <canvas id="pelota" />
-            <canvas id="graficoOfBall" />
+            <canvas
+              id="canvasPrincipal"
+              onMouseDown={(event)=>{
+                compareImgs(event,imgOriginal,arrayImgs)}
+              }
+              width="auto" height="auto"/>
+            <canvas id="canvasPelota" />
+            <canvas id="calculatedGraph" />
             <script>
 
               {
                 window.onload = function() {
 
-                  window.loadImage=function(url,contexto, callback){
+                  window.loadImage=function(url,contexto,callback){
                     let img = new Image();
-
                     img.onload = function(){
                       contexto.drawImage(img, 0, 0);
 
@@ -31,48 +35,20 @@ export class Comparar extends Component{
                     img.src = url;
                   }
 
-                  var c = document.getElementById("canvas");
+                  var c = document.getElementById("canvasPrincipal");
                   var ctx = c.getContext("2d");
 
-                  var c2 = document.getElementById("pelota");
+                  var c2 = document.getElementById("canvasPelota");
                   var ctx2 = c2.getContext("2d");
                   let imgDataArray = [];
-                  loadImage (pelota, ctx2, function(imgDataMod) {
+
+                  loadImage (arrayImgs[0], ctx2, function(imgDataMod) {
                     imgDataArray.push(imgDataMod);
+
                     loadImage (imgOriginal, ctx, function(imgDataOriginal) {
-                      imgDataArray.push(imgDataOriginal);
-                      compareReturn = compareImgs(imgDataArray);
-
-                      let {datadiff,max} = compareReturn;
-
-                      console.log(datadiff);
-                      // max = Math.max(datadiff);
-                      console.log(max);
-
-                      let graficoOfBall = document.getElementById("graficoOfBall");
-                      let contextGrafico = graficoOfBall.getContext('2d');
-                      let imgBnW = contextGrafico.createImageData(320,200);
-
-
-                      for ( var i = 0; i < datadiff.length; i ++ ) {
-                        if (max/divisorUmbral < datadiff[i]){
-                          imgBnW.data[i*4]=0;
-                          imgBnW.data[i*4+1]=0;
-                          imgBnW.data[i*4+2]=0;
-                          imgBnW.data[i*4+3]=255;
-                          getDistance(mouseX,mouseY,getBlackDotsCoord(i*4));
-                          //CALCULAR LA DISTANCIA SEGUN CLICK getDistance
-                        }
-                        else{
-                          imgBnW.data[i*4]=255;
-                          imgBnW.data[i*4+1]=255
-                          imgBnW.data[i*4+2]=255;
-                          imgBnW.data[i*4+3]=0;
-                        }
-
-                      }
-                      contextGrafico.putImageData(imgBnW,0,0);
                     });
+
+
                   });
 
 
