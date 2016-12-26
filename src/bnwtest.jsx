9c913a0fc39a0726c35767285261e91669b8881c
heberlipe <heberlipe@hotmail.com>
2016-12-26 11:36:02 -0300

    BnW canvas working (missing size)

---
+++ src/bnwtest.jsx
@@ -0,0 +1,114 @@
+var divisorUmbral = 20;
+var imageDataArray = [];
+
+//hardcodeados
+var imgWidth = 2440;
+var imgHeight = 1639;
+
+export const getBnWImages = (originalImg, answersArray, umbral) => {
+  if(umbral !== undefined){
+    divisorUmbral = umbral;
+  }
+  return initCompare(originalImg,answersArray);
+}
+
+const initCompare = (originalImg, answersArray) =>{
+  let imgDataArray= [];
+  return new Promise( (resolve,reject) => {
+    loadBnWImages(originalImg,answersArray,imgDataArray, 0).then(
+      (bnwDataArray) => {
+        resolve({
+          dataArray: bnwDataArray,
+          width: imgWidth,
+          height: imgHeight
+        });
+      }
+    );
+  })
+}
+
+const loadBnWImages = (originalImg, answersArray, imgDataArray, index)  =>{
+  return new Promise( (resolve,reject) => {
+    if (index>=answersArray.length){ // si es el ultimo
+      let img = new Image();
+      let context = document.createElement("canvas").getContext('2d');
+      img.onload = () => {
+        context.drawImage(img, 0, 0);
+        let originalImgData = context.getImageData(0,0,img.width,img.height).data;
+        resolve(getBnWDataArray(originalImgData, imgDataArray));
+      }
+      img.src = originalImg;
+    }else{ //si no es el ultimo
+      let img = new Image();
+      let context = document.createElement("canvas").getContext('2d');
+      img.onload = () => {
+        context.drawImage(img, 0, 0);
+        imgDataArray.push({
+            dataArray: context.getImageData(0,0,img.width,img.height).data,
+            width: img.width,
+            height: img.height
+          });
+        imgWidth=img.width;
+        imgHeight=img.height;
+        loadBnWImages(originalImg, answersArray,imgDataArray, index+1).then(
+          (resolved) => {
+            resolve(resolved);
+          }
+        );
+      };
+      img.src = answersArray[index].url;
+    }
+  });
+}
+
+const getBnWDataArray = (dataArrayOriginal, imgDataArray) => {
+  let bnwDataArray = [];
+
+  for (let i=0; i<imgDataArray.length;i++){
+    let dataArrayAnswers = imgDataArray[i].dataArray;
+    let {datadiff, max} = getDiffDotsAndMax(dataArrayOriginal,dataArrayAnswers);
+    bnwDataArray.push(getBnWData(datadiff, max));
+  }
+  return bnwDataArray
+}
+
+const getDiffDotsAndMax = (imgDataOriginal, imgDataMod) => {
+  let datadiff = [];
+  let max = 0;
+
+  for ( var i = 0; i < imgDataMod.length; i += 4 ) {
+    let diffPixel = (imgDataMod[i]-imgDataOriginal[i]) * (imgDataMod[i]-imgDataOriginal[i]) +
+    (imgDataMod[i + 1]-imgDataOriginal[i + 1]) * (imgDataMod[i + 1]-imgDataOriginal[i + 1]) +
+    (imgDataMod[i + 2]-imgDataOriginal[i + 2]) * (imgDataMod[i + 2]-imgDataOriginal[i + 2]) +
+    (imgDataMod[i + 3]-imgDataOriginal[i + 3]) * (imgDataMod[i + 3]-imgDataOriginal[i + 3]);
+
+    datadiff.push(diffPixel);
+    max = Math.max(max,diffPixel);
+
+  }
+  console.log("max= "+max);
+  return {datadiff, max};
+}
+
+const getBnWData = (imgDataDiff,max) =>{
+  let context = document.createElement("canvas").getContext('2d');
+  let imgBnW = context.createImageData(imgWidth,imgHeight);
+  let x = 0;
+  for ( var i = 0; i < imgDataDiff.length; i ++ ) {
+
+    if (max/divisorUmbral < imgDataDiff[i]){
+      imgBnW.data[i*4]=0;
+      imgBnW.data[i*4+1]=0;
+      imgBnW.data[i*4+2]=0;
+      imgBnW.data[i*4+3]=255;
+    }
+    else{
+      imgBnW.data[i*4]=255;
+      imgBnW.data[i*4+1]=255
+      imgBnW.data[i*4+2]=255;
+      imgBnW.data[i*4+3]=0;
+    }
+  }
+  console.log(imgBnW);
+  return imgBnW.data;
+}
---
+++ src/compararBnw.jsx
@@ -0,0 +1,119 @@
+import React,{ Component } from 'react';
+import {getBnWImages} from './bnwTest.jsx';
+
+const originalMessi = '/assets/messi_original.small.jpeg';
+const arrayMessi = [
+{
+  url: "/assets/messi_bocha.small.jpeg",
+  texto: 'bocha'
+},
+{
+  url: "assets/messi_cuerpo.small.jpeg",
+  texto: 'messi'
+},
+{
+  url: "assets/messi_fondo.small.jpeg",
+  texto: 'fondo'
+}
+];
+const originalJugadores = '/assets/procesada_photoshop.jpg';
+const arrayJugadores = [
+{
+  url: "/assets/messi.jpg",
+  texto: 'messi'
+},
+{
+  url: "/assets/neymar.jpg",
+  texto: 'neymar'
+},
+{
+  url: "/assets/suarez.jpg",
+  texto: 'suarez'
+},
+{
+  url: "/assets/fondo.jpg",
+  texto: 'fondo'
+}
+];
+const divisorUmbral = 20;
+
+const arrayImgs = arrayJugadores;
+const imgOriginal = originalJugadores;
+
+export class CompararBnw extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      bnwImages : {}
+    }
+    this.actualizarEstado = this.actualizarEstado.bind(this);
+    this.loadImage = this.loadImage.bind(this);
+    this.updateCanvas = this.updateCanvas.bind(this);
+  }
+
+  componentDidMount(){
+      let {loadImage} = this;
+      let imgOriginalUrlArray = [];
+      let arrayContext = [];
+      imgOriginalUrlArray.push(imgOriginal);
+      arrayImgs.map( (value, index) => {
+          arrayContext.push(document.getElementById(index).getContext('2d'));
+      })
+      loadImage (imgOriginalUrlArray, arrayContext);
+  }
+
+  loadImage = (arrayImgs,contextos) => {
+    contextos.map( (contexto) => {
+          let img = new Image();
+          img.onload = () => {
+            contexto.drawImage(img, 0, 0);
+          };
+          img.src = arrayImgs[0];
+      });
+  }
+
+  actualizarEstado(){
+    getBnWImages(imgOriginal,arrayImgs).then(
+      (bnwImages) => {
+        console.log(bnwImages);
+        this.setState({
+          bnwImages : bnwImages
+        });
+      }
+    );
+  }
+
+  componentDidUpdate(){
+    this.updateCanvas();
+  }
+
+  updateCanvas = () => {
+    let {bnwImages} = this.state;
+    arrayImgs.map( (value,index) => {
+      let canvas = document.getElementById(index);
+      let contexto = canvas.getContext('2d');
+      let newImageData = contexto.createImageData(bnwImages.width,bnwImages.height);
+      let x = 0;
+      bnwImages.dataArray[index].map((value,index)=>{
+        newImageData.data[index]=value;
+      })
+      contexto.putImageData(newImageData,0,0);
+    })
+  }
+
+  render(){
+    let {actualizarEstado} = this;
+    	return(
+    		<div>
+          <button onClick = {()=>{actualizarEstado()}}>Get Img BnW</button>
+          {
+            arrayImgs.map( (value, index) => {
+              return (
+                <canvas id={index} key={index}></canvas>
+              );
+            })
+          }
+        </div>
+    	);
+  }
+}
